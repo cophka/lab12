c21afdc12cbf8dc6e5075beb92a4598eff671ddf
Marian Koniuszko surf <cophka@cophka.com>
2018-11-23 12:59:40 +0100

    add empty project

---
+++ Wyklad_12/SystemGlobalizationPrzyklady/SystemGlobalizationPrzyklady/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SystemGlobalizationPrzyklady
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKultury = new System.Windows.Forms.Button();
+            this.btnRegion = new System.Windows.Forms.Button();
+            this.btnModyfikacjaFormatuDaty = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCiagiZnakow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKultury
+            // 
+            this.btnKultury.Location = new System.Drawing.Point(45, 12);
+            this.btnKultury.Name = "btnKultury";
+            this.btnKultury.Size = new System.Drawing.Size(255, 23);
+            this.btnKultury.TabIndex = 0;
+            this.btnKultury.Text = "Wyświetl informacje o kulturach";
+            this.btnKultury.UseVisualStyleBackColor = true;
+            this.btnKultury.Click += new System.EventHandler(this.btnKultury_Click);
+            // 
+            // btnRegion
+            // 
+            this.btnRegion.Location = new System.Drawing.Point(45, 55);
+            this.btnRegion.Name = "btnRegion";
+            this.btnRegion.Size = new System.Drawing.Size(255, 23);
+            this.btnRegion.TabIndex = 1;
+            this.btnRegion.Text = "Wyświetl informacje o regionie";
+            this.btnRegion.UseVisualStyleBackColor = true;
+            this.btnRegion.Click += new System.EventHandler(this.btnRegion_Click);
+            // 
+            // btnModyfikacjaFormatuDaty
+            // 
+            this.btnModyfikacjaFormatuDaty.Location = new System.Drawing.Point(45, 94);
+            this.btnModyfikacjaFormatuDaty.Name = "btnModyfikacjaFormatuDaty";
+            this.btnModyfikacjaFormatuDaty.Size = new System.Drawing.Size(255, 23);
+            this.btnModyfikacjaFormatuDaty.TabIndex = 2;
+            this.btnModyfikacjaFormatuDaty.Text = "Modyfikuj format daty";
+            this.btnModyfikacjaFormatuDaty.UseVisualStyleBackColor = true;
+            this.btnModyfikacjaFormatuDaty.Click += new System.EventHandler(this.btnModyfikacjaFormatuDaty_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Separator liczb dziesiętnych i grup liczb";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCiagiZnakow
+            // 
+            this.btnCiagiZnakow.Location = new System.Drawing.Point(45, 178);
+            this.btnCiagiZnakow.Name = "btnCiagiZnakow";
+            this.btnCiagiZnakow.Size = new System.Drawing.Size(255, 23);
+            this.btnCiagiZnakow.TabIndex = 4;
+            this.btnCiagiZnakow.Text = "Porównaj 2 ciągi znaków(0 - identyczne)";
+            this.btnCiagiZnakow.UseVisualStyleBackColor = true;
+            this.btnCiagiZnakow.Click += new System.EventHandler(this.btnCiagiZnakow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 223);
+            this.Controls.Add(this.btnCiagiZnakow);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnModyfikacjaFormatuDaty);
+            this.Controls.Add(this.btnRegion);
+            this.Controls.Add(this.btnKultury);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wykład 12 - System.Globalization";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKultury;
+        private System.Windows.Forms.Button btnRegion;
+        private System.Windows.Forms.Button btnModyfikacjaFormatuDaty;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCiagiZnakow;
+
+    }
+}
+
---
+++ Wyklad_12/SystemGlobalizationPrzyklady/SystemGlobalizationPrzyklady/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Dodanie przestrzenie nazw do obsługi Globalizacji i Wątków
+using System.Globalization;
+using System.Threading;
+
+namespace SystemGlobalizationPrzyklady
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKultury_Click(object sender, EventArgs e)
+        {
+            //Utworzenie zmiennej tablicowej klasy CultureInfo przechowującej dane o kulturach dostepnych w systemie
+            CultureInfo[] kultury = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            //Dla każdej zmiennej w tablicy wyświetl informacje o kulturze
+            foreach (CultureInfo zebrane_kultury in kultury)
+            {
+                MessageBox.Show(zebrane_kultury.Name.ToString());
+            }
+        }
+
+        private void btnRegion_Click(object sender, EventArgs e)
+        {
+            //Utworzenie zmiennej klasy RegionInfo przechowującej informacje o danej kulturze
+            RegionInfo regionFrancja = new RegionInfo("FR");
+            //Wyświetlenie szczegółowych informacji o danej kulturze
+            MessageBox.Show("Name: " + regionFrancja.Name);
+            MessageBox.Show("Display name: " + regionFrancja.DisplayName);
+            MessageBox.Show("Symbol (pol. waluta): " + regionFrancja.CurrencySymbol);
+        }
+
+        private void btnModyfikacjaFormatuDaty_Click(object sender, EventArgs e)
+        {
+            //Utworzenie zmiennej klasy DateTimeFormatInfo umożliwiającej formatowanie daty i ustawienie jej na format brytyjski
+            DateTimeFormatInfo dateFormat1 = new CultureInfo("en-GB", false).DateTimeFormat;
+            //Utworzenie zmiennej klasy DateTimeFormatInfo umożliwiającej formatowanie daty i pobranie jej wartości dla bieżącego wątku
+            DateTimeFormatInfo dateFormat2 = Thread.CurrentThread.CurrentCulture.DateTimeFormat;
+
+            //Wyświetlenie zawartości powyższych zmiennych
+            MessageBox.Show("dateFormat1 : "+ dateFormat1.ShortDatePattern);
+            MessageBox.Show("dateFormat2 : " + dateFormat2.ShortDatePattern);
+
+            //Modyfikacja skrótowych oznaczeń miesięcy w zmiennej datefFormat1
+            dateFormat1.AbbreviatedMonthNames = new string[] { "Jn", "Fb", "Marzec OK", "Ap", "My", "Jn", "Jy", "Au", "Se", "Oc", "Nv", "De", "" };
+
+            //Utworzenie ciągu znakowego currentDate przechowującą sformatowaną wartość zmiennej dateFormat1
+            string currentDate = DateTime.Now.ToString("dd-MMM-yyyy",dateFormat1);
+
+            //Wyświetlenie zawartości zmiennej currentDate
+            MessageBox.Show(currentDate);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Utworzenie zmiennej numberFormat do której wpisujemy informacje o formatowaniu liczb pobranych z bieżącego wątku 
+            NumberFormatInfo numberFormat = Thread.CurrentThread.CurrentCulture.NumberFormat;
+
+            //Wyświetlenie informacji o separatorze liczb dzisiętnych i separatorze grupy liczb pobranych z bieżącego wątku 
+            MessageBox.Show("Separator liczb dziesiętnych: " + numberFormat.NumberDecimalSeparator);
+            MessageBox.Show("Separator grup liczb: " + numberFormat.NumberGroupSeparator);
+        }
+
+        private void btnCiagiZnakow_Click(object sender, EventArgs e)
+        {
+            string string1 = "strings test";
+            string string2 = "Strings test";
+
+            //Utworzenie zmiennej stringComp klasy CompareInfo umożliwiającej porównywanie ciągów znakowych zgodnie z zasadami obowiązującymi w danej kulturze
+            CompareInfo stringComp = Thread.CurrentThread.CurrentCulture.CompareInfo;
+            //Wyświetlenie wyników porównania bez opcji lub z opcją ingnorowania dużych liter (Porównanie 2)
+            MessageBox.Show("Porównanie 1: " + stringComp.Compare(string1, string2, CompareOptions.None));
+            MessageBox.Show("Porównanie 2: " + stringComp.Compare(string1, string2, CompareOptions.IgnoreCase));
+
+            string1 = "strings test";
+            string2 = "string's test";
+
+            //Wyświetlenie wyników porównania z opcją ignorowania symboli
+            MessageBox.Show("Porównanie 3: " + stringComp.Compare(string1, string2, CompareOptions.IgnoreSymbols));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
